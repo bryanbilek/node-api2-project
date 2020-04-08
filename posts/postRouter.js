2e25678799c6b03909473d32ccce46af435da480
bryanbilek <you@example.com>
2020-04-08 15:18:51 -0500

    still a bug where it works but postman says errormessage

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -5,41 +5,57 @@ const router = express.Router();
 const Posts = require("../data/db.js");
 
 //POST /posts/ does post but response tells me error
-//POST /posts/:id/comments doesn't work at all
 //PUT /posts/:id updates but gives me error message
 
 //POST = /api/posts/
-router.post('/', (req, res) => {
+// router.post('/', (req, res) => {
+//     Posts.insert(req.body)
+//         .then(post => {
+//             if (post.title === undefined || post.contents === undefined) {
+//                 res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+//             } else {
+//                 res.status(201).json(post);
+//             }
+//         })
+//         .catch(error => {
+//             res.status(500).json({ error: "There was an error while saving the post to the database" });
+//         });
+// });
+
+router.post("/", (req, res) => {
     Posts.insert(req.body)
-        .then(post => {
-            if (!post.title || !post.contents) {
-                res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
-            } else {
-                res.status(201).json(post);
-            }
-        })
-        .catch(error => {
-            res.status(500).json({ error: "There was an error while saving the post to the database" });
-        });
-});
+    .then(post => {
+        if (!post || !post.title || !post.contents) {
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+        } else {
+            res.status(201).json(post);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: "There was an error while saving the post to the database" });
+    })
+})
 
 //POST = /api/posts/:id/comments
-router.post("/:id/comments", async (req, res) => {
-    try {
-        const post = await Posts.findCommentById(+req.params.id);
-        console.log("post", post);
-        if (!post.length) {
+router.post("/:id/comments", (req, res) => {
+    Posts.findCommentById(req.params.id)
+    .then(post => {
+        if (post) {
+            Posts.insertComment(req.body)
+            .then(comment => {
+                if (req.body.text) {
+                    res.status(201).json(comment);
+                } else {
+                    res.status(400).json({ errorMessage: "Please provide text for the comment." });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ error: "The comments information could not be retrieved." });
+            })
+        } else {
             res.status(404).json({ message: "The post with the specified ID does not exist." });
-        } else if (post.text === undefined) {
-            res.status(400).json({ errorMessage: "Please provide text for the comment." });
-          }
-        const comments = await Posts.insertComment(Number(req.params.id))
-        console.log("comments", comments);
-        res.status(201).json(comments);
-    } catch (err) {
-        console.log("err", err)
-        res.status(500).json({ error: "The comments information could not be retrieved." });
-    }
+        }
+    })
 });
 
 //GET = /api/posts/
