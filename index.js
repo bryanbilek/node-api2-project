f0f46aaae17736baa293fabd5aac551edf4e8af9
bryanbilek <you@example.com>
2020-04-09 14:14:44 -0500

    added ability to deploy on heroku

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ server.get("/", (req, res) => {
   res.send("API is running.....");
 });
 
-server.listen(5000, () => {
-  console.log('Server Running on http://localhost:5000');
+// server.listen(5000, () => {
+//   console.log('Server Running on http://localhost:5000');
+// });
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
